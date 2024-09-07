be6cc73088ff40d1829e6dbde4b24ed6a9095e0f
Joao Henrique Toigo <jhtoigo@acad.ftec.com.br>
2024-09-07 09:59:45 -0300

    chore: template and asg vars

--- variables.tf
+++ variables.tf
@@ -41,6 +41,16 @@ variable "load_balancer_type" {}
 
 ## ECS General
 
+variable "node_ami" {}
+
 variable "node_instance_type" {}
 
-variable "node_ami" {}
+variable "node_volume_size" {}
+
+variable "node_volume_type" {}
+
+variable "cluster_on_demand_min_size" {}
+
+variable "cluster_on_demand_max_size" {}
+
+variable "cluster_on_demand_desired_size" {}
