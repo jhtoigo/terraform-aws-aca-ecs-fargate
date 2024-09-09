abf9764f0bc77b8707219d9d3ac4e650fd4b2ff6
Joao Henrique Toigo <jhtoigo@acad.ftec.com.br>
2024-09-09 16:09:20 -0300

    fix: disable ssm parameter if no alb active

--- modules/ecs_cluster/parameter.tf
+++ modules/ecs_cluster/parameter.tf
@@ -1,11 +1,13 @@
 resource "aws_ssm_parameter" "lb_arn" {
+  count = var.lb_active ? 1 : 0
   name  = format("/%s/ecs/lb/id", var.project_name)
-  value = aws_lb.main.arn
+  value = aws_lb.main[count.index].arn
   type  = "String"
 }
 
 resource "aws_ssm_parameter" "lb_listener" {
+  count = var.lb_active ? 1 : 0
   name  = format("/%s/ecs/lb/listenernet", var.project_name)
-  value = aws_lb_listener.main.id
+  value = aws_lb_listener.main[count.index].id
   type  = "String"
 }
